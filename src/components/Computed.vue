bbf4c34fc1efe7567b256cd58f979cb2e56df99a
5z5 <13755911489@163.com>
2022-10-09 16:35:30 +0800

    one

---
+++ src/components/Computed.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive, computed } from 'vue'
+
+const author = reactive({
+    name: 'John Doe',
+    books: [
+        'Vue 2',
+        'Vue 3',
+        'Vue 4'
+    ]
+})
+
+// 一个计算属性 ref
+const publishedBooksMessage = computed(() => {
+    return author.books.length > 0 ? 'Yes' : 'No'
+})
+</script>
+
+<template>
+    <p>Has published books:</p>
+    <span>{{ publishedBooksMessage }}</span>
+</template>
